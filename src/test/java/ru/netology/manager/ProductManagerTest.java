8039ab84f8bbf02cd73a404a1f5fb945e060a280
Nick <zxmadjik@mail.ru>
2021-10-25 22:56:41 +0400

    improves

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -1,5 +1,6 @@
 package ru.netology.manager;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
@@ -8,21 +9,26 @@ import ru.netology.repository.ProductRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ProductManagerTest {
+public class ProductManagerTest {
     Product one = new Book(1, "testBook", 50, "Petrov");
     Product two = new Smartphone(2, "TestPhone", 1000, "Nokia");
     Product three = new Smartphone(3, "TestPhone", 1000, "LG");
     Product four = new Book(4, "testBook", 50, "Petrov");
+    ProductRepository repository = new ProductRepository();
+    ProductManager manager = new ProductManager(repository);
 
-
-    @Test
-    public void shouldSearchByName() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager(repository);
+    @BeforeEach
+    public void setUp() {
         manager.add(one);
         manager.add(two);
         manager.add(three);
         manager.add(four);
+    }
+
+
+    @Test
+    public void shouldSearchByName() {
+
         Product[] actual = manager.searchBy("testBook");
         Product[] expected = new Product[]{one, four};
         assertArrayEquals(actual, expected);
@@ -30,15 +36,24 @@ class ProductManagerTest {
     }
 
     @Test
-    public void shouldSearchByAuthorOrMaker() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager(repository);
-        manager.add(one);
-        manager.add(two);
-        manager.add(three);
-        manager.add(four);
+    public void shouldSearchByMaker() {
+
         Product[] actual = manager.searchBy("LG");
         Product[] expected = new Product[]{three};
+        assertArrayEquals(actual, expected);
+    }
+    @Test
+    public void shouldSearchByAuthor() {
+
+        Product[] actual = manager.searchBy("Petrov");
+        Product[] expected = new Product[]{one, four};
+        assertArrayEquals(actual, expected);
+    }
+    @Test
+    public void shouldSearchByAuthorFindNothing() {
+
+        Product[] actual = manager.searchBy("Ivanov");
+        Product[] expected = new Product[]{};
         assertArrayEquals(actual, expected);
     }
 }
